16e1ed10a086c5a279a6024aa5446a6e26e30538
Chen Liu <cliu110@illinois.edu>
2018-05-01 09:48:38 -0500

    Try to add music.

--- app/src/main/java/edu/illinois/cs/cs125/mp7_text_adventure_game/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/mp7_text_adventure_game/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs.cs125.mp7_text_adventure_game;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
     /** Default tag for mp7. */
     private static final String TAG = "Mp7:Main";
 
+    MediaPlayer mySong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mySong = MediaPlayer.create(MainActivity.this,R.raw)
         final Button enter = findViewById(R.id.enterText);
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"text enter button clicked");
             }
         });
+
+
+    }
+
+    public void playMusic(View v) {
+
 
     }
 
